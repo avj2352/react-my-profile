85629f374ca52cd639ed86f5202d56e311c51314
Jingade <pramod.jingade@philips.com>
2018-12-19 20:52:10 +0530

    completed circle loader responsiveness for mobile devices

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -3,8 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 // Components
 import CircleLoader from './../components/circle-loader/circle-loader.jsx';
-//Import context
+//Context
 import {AppProvider, AppContext} from './../common/AppContext';
+// Material Components
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 
 class App extends Component {
@@ -12,10 +15,13 @@ class App extends Component {
     const header = <AppContext.Consumer>{(context) => context.header}</AppContext.Consumer>
     return (
       <AppProvider>
-      <div className="app">        
-          <h1 className="app-header">Hi, I am <span className="highLight">PRAMOD JINGADE</span></h1>                  
-          <CircleLoader></CircleLoader>          
-          <h2 className="app-subtitle">Avid UI / UX Designer, based in Bangalore</h2>
+      <div className="page">
+        <div className="app">        
+            <h1 className="app-header">Hi, I am <span className="highLight">PRAMOD JINGADE</span></h1>                  
+            <CircleLoader></CircleLoader>          
+            <h2 className="app-subtitle">Avid UI / UX Designer, based in Bangalore</h2>
+        </div>      
+        <Button variant="contained" color="secondary" className="medium-size">ABOUT ME</Button>
       </div>
       </AppProvider>
     );
